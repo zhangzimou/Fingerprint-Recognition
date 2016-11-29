7c69fd10631cf53cca545040c99c123e1fbc6a9a
WxFang <wenxinfang97@gmail.com>
2016-11-29 17:55:28 -0500

    Update __init__.py

--- basic/__init__.py
+++ basic/__init__.py
@@ -33,9 +33,9 @@ for file in os.listdir(path):
 	filename[count] = file
 	count += 1
 #search all filenames for same images and acheive correct probability
-matchedPoints_max = np.array([80,80])
+matchedPoints_max = np.zeros([80,80])
 for i in range(80):
-	for j in range(80):
+	for j in range(i+2,80-i-1):
 		#input
 		img1 = cv2.imread(path+'/'+filename[i],0)
 		img2 = cv2.imread(path+'/'+filename[j],0)
@@ -45,23 +45,19 @@ for i in range(80):
 		else:
 			print "They are from different fingerprint"
 		#preprocess for images
-		img_origin1,imgfore1=pre.segmentation(img1)
-		img_origin2,imgfore2=pre.segmentation(img2)
-		blockSize=16
-		theta1=pre.calcDirection(img_origin1,blockSize)
-		theta2=pre.calcDirection(img_origin2,blockSize)
-		wl1=pre.calcWl(img_origin1,blockSize)
-		wl2=pre.calcWl(img_origin2,blockSize)
-		img_enhance1=pre.GaborFilter(img_origin1,blockSize,wl1,np.pi/2-theta1)
-		img_enhance1[np.where(imgfore1==255)]=255
-		img_thin1=pre.thinning(basic.binarize(img_enhance1))
-		img_enhance2=pre.GaborFilter(img_origin2,blockSize,wl2,np.pi/2-theta2)
-		img_enhance2[np.where(imgfore2==255)]=255
-		img_thin2=pre.thinning(basic.binarize(img_enhance2))
-        matchedPoints_max[i][j] = minutiaeMatch(img_thin1, img_thin2, imgfore1, imgfore2)
-        print matchedPoints_max[i][j]
+		img_seg1,imgfore1=pre.segmentation(img1)
+		img_seg2,imgfore2=pre.segmentation(img2)
+		imgE1=enhance(img1)
+		imgE2=enhance(img2)
+		imgB1=basic.binarize(imgE1)
+		imgB2=basic.binarize(imgE2)
+		imgT1=pre.thinning(imgB1)
+		imgT2=pre.thinning(imgB2)
+		print "Matched points:"
+		matchedPoints_max[i][j] = minutiaeMatch(imgT1, imgT2, imgfore1, imgfore2)
+		print matchedPoints_max[i][j]
 
 
 
-end=time.clock()
+end = time.clock()
 print end-start
