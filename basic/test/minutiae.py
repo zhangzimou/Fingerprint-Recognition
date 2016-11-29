17f77bc35aab53622b196c11f725bd378cf49fef
Zimou Zhang <zhangzimouzzm@gmail.com>
2016-11-29 00:04:47 -0500

    minutiae test

--- basic/test/minutiae.py
+++ basic/test/minutiae.py
@@ -6,6 +6,9 @@ Created on Mon Nov 28 12:02:15 2016
 @author: zhangzimou
 """
 
+import pyximport
+pyximport.install()
+
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -24,18 +27,27 @@ from basic import block_view
 FVC0='/home/zhangzimou/Desktop/code_lnk/database/FVC2000/'
 FVC2='/home/zhangzimou/Desktop/code_lnk/database/FVC2002/'
 FVC4='/home/zhangzimou/Desktop/code_lnk/database/FVC2004/'
-path=FVC4+'DB1_B/'
+path=FVC4+'DB4_B/'
 
 blockSize=8
 
-img=cv2.imread(path+'106_1.tif',0)
+img=cv2.imread(path+'102_1.tif',0)
 
 img_seg,imgfore=pre.segmentation(img)
 
 img_en=enhance(img)
-
+imgB=basic.binarize(img_en)
+imgT=pre.thinning(imgB)
 
 plt.figure()
-imshow(img)
+imshow(img_seg)
 plt.figure()
-imshow(img_en)
+plt.imshow(imgB,cmap='gray')
+plt.figure()
+plt.imshow(imgT,cmap='gray')
+
+
+ending,bifur,theta1,theta2=minutiaeExtract(imgT,imgfore)
+plt.plot(ending.T[1],ending.T[0],'b.',bifur.T[1],bifur.T[0],'r.')
+plt.quiver(ending.T[1],ending.T[0],np.cos(theta1),np.sin(-theta1),color='b',width=0.003)
+plt.quiver(bifur.T[1],bifur.T[0],np.cos(theta2),np.sin(-theta2),color='r',width=0.003)
